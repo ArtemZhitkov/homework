695527e93ae3a0f7c93cdfe4c69e4bb460507c1e
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-02 00:04:19 +0500

    fixes mypy

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -8,7 +8,7 @@ from src.masks import get_mask_account, get_mask_card_number
                                                            ('', None),
                                                            (7158300734726758, '7158 30** **** 6758'),
                                                            ('71583007347267581', None)])
-def test_get_mask_card_number(card_number, mask_card_number):
+def test_get_mask_card_number(card_number: str, mask_card_number: str) -> None:
     assert get_mask_card_number(card_number) == mask_card_number
 
 
@@ -17,5 +17,5 @@ def test_get_mask_card_number(card_number, mask_card_number):
                                                           (64686473678894779589, '**9589'),
                                                           ('646864736788947795890', None),
                                                           ('646864736788947795', None)])
-def test_get_mask_account(account_number, masc_account):
+def test_get_mask_account(account_number: str, masc_account: str) -> None:
     assert get_mask_account(account_number) == masc_account
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,14 +3,15 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize('card_number, mask_account', [('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
-                                                       ('Счет 64686473678894779589', 'Счет **9589'),
-                                                       ('', None),
-                                                       ('Счет 18986161661587457', None),
-                                                       ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
-                                                       ('MasterCard 715830073472675844', None),
-                                                       ('MasterCard 71583007347267', None)])
-def test_mask_account_card(card_number, mask_account):
+@pytest.mark.parametrize('card_number, mask_account',
+                         [('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
+                          ('Счет 64686473678894779589', 'Счет **9589'),
+                          ('', None),
+                          ('Счет 18986161661587457', None),
+                          ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
+                          ('MasterCard 715830073472675844', None),
+                          ('MasterCard 71583007347267', None)])
+def test_mask_account_card(card_number: str, mask_account: str) -> None:
     assert mask_account_card(card_number) == mask_account
 
 
@@ -19,5 +20,5 @@ def test_mask_account_card(card_number, mask_account):
                                                ('30-05-2020', '30.05.2020'),
                                                ('', 'Не верный формат даты'),
                                                ('2024-03', 'Не верный формат даты')])
-def test_get_date(date, format_date):
+def test_get_date(date:str, format_date: str) -> None:
     assert get_date(date) == format_date
