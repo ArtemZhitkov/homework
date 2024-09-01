ec2bde0fd90374242aa66d4d9248eda3e6094b47
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-01 20:09:10 +0500

    fixes mask.py

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,7 @@ def get_mask_card_number(card_number: str) -> str | None:
     """Функция принимает на вход номер карты и возвращает ее маску.
     Номер карты замаскирован и отображается в формате
     XXXX XX** **** XXXX, где X — это цифра номера."""
+    card_number = str(card_number)
     if len(card_number) == 16:
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
     else:
@@ -12,7 +13,11 @@ def get_mask_account(account_number: str) -> str | None:
     """Функция принимает на вход номер счета и возвращает его маску.
     Номер счета замаскирован и отображается в формате **XXXX,
      где X — это цифра номера."""
+    account_number = str(account_number)
     if len(account_number) == 20:
         return f"**{account_number[-4:]}"
     else:
         return None
+
+
+print(get_mask_account('64686473678894779589'))
