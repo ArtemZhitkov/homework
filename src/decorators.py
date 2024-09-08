d1ef1c4facd35f61a35d87e99e9ae86ddaa72610
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-08 22:41:15 +0500

    fixes decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,28 +1,29 @@
-from datetime import time
+from datetime import datetime
 from functools import wraps
 
 
 def log(filename=None):
     def logging(func):
-        @wraps
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                start = time()
+                start = datetime.now()
                 result = func(*args, **kwargs)
-                stop = time()
+                stop = datetime.now()
                 if filename:
-                    path = r'C:\Users\artem_zhitkov\PycharmProjects\homework\logs' + filename + '.txt'
+                    path = r'C:\Users\artem_zhitkov\PycharmProjects\homework\logs\.' + filename + '.txt'
                     with open(path, 'a', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} start - {start} \n {func.__name__} - ok \n'
-                                   f' {func.__name__} stop - {stop}')
+                        file.write(f'{func.__name__} start - {start}\n{func.__name__} - ok\n'
+                                   f'{func.__name__} stop - {stop}.\n')
                 else:
-                    print(f'{func.__name__} start - {start} \n {func.__name__} - ok \n {func.__name__} stop - {stop}')
+                    print(f'{func.__name__} start - {start} \n{func.__name__} - ok \n{func.__name__} stop - {stop}')
             except Exception as error:
                 if filename:
+                    path = r'C:\Users\artem_zhitkov\PycharmProjects\homework\logs\.' + filename + '.txt'
                     with open(path, 'a', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}')
+                        file.write(f'{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}.\n')
                 else:
-                    print(file.write(f'{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}'))
+                    print(f'{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}')
                 raise
 
             return result
