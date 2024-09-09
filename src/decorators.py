7eb385ff6956f81e934f1d4ff78dfe829010e0d5
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-09 19:29:08 +0500

    fixes decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,31 +1,35 @@
-from datetime import datetime
 from functools import wraps
+from typing import Any, Callable, Optional
 
 
-def log(filename=None):
-    def logging(func):
+def log(filename: Optional[str] = None) -> Callable:
+    """Декоратор, который автоматически логирует начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки.
+    Декоратор принимает необязательный аргумент filename с расширением ".txt",
+    который определяет, куда будут записываться логи (в файл или в консоль)."""
+
+    def logging(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
-                start = datetime.now()
+                start = f"{func.__name__} start"
                 result = func(*args, **kwargs)
-                stop = datetime.now()
+                stop = f"{func.__name__} stop"
                 if filename:
-                    path = r'C:\Users\artem_zhitkov\PycharmProjects\homework\logs\.' + filename + '.txt'
-                    with open(path, 'a', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} start - {start}\n{func.__name__} - ok\n'
-                                   f'{func.__name__} stop - {stop}.\n')
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} - {start}\n{func.__name__} - ok\n" f"{func.__name__} - {stop}.\n")
                 else:
-                    print(f'{func.__name__} start - {start} \n{func.__name__} - ok \n{func.__name__} stop - {stop}')
+                    print(f"{func.__name__} start - {start} \n{func.__name__} - ok \n{func.__name__} stop - {stop}")
             except Exception as error:
                 if filename:
-                    path = r'C:\Users\artem_zhitkov\PycharmProjects\homework\logs\.' + filename + '.txt'
-                    with open(path, 'a', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}.\n')
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}.\n")
                 else:
-                    print(f'{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}')
+                    print(f"{func.__name__} error: {error} with args: {args} and kwargs: {kwargs}")
                 raise
 
             return result
+
         return wrapper
+
     return logging
