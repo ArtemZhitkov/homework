1552d0bc3fd2debe31809631208cf7dbc486d790
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-21 20:41:43 +0500

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -107,3 +107,22 @@ def transaction_in_rub():
             "from": "Счет 44812258784861134719",
             "to": "Счет 74489636417521191160"
         }
+
+
+@pytest.fixture
+def transaction_in_usd():
+    return {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
