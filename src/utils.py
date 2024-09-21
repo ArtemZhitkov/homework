c89deae512f9f02d14e45bced1f3406d9ff28607
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-21 20:42:13 +0500

    fixes

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import List, Any
+from typing import Any, List
 
 from src.external_api import currency_conversion_in_rub
 
@@ -25,18 +25,16 @@ def get_data_from_json(path: str) -> List[dict] | Any:
         return []
 
 
-def get_amount_in_rub(transaction: dict, currency: str = "RUB"):
+def get_amount_in_rub(transaction: dict) -> float | str:
     """Функция, которая принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях.
     Если транзакция была в USD или EUR,
     происходит обращение к внешнему API для получения текущего курса валют и
      конвертации суммы операции в рубли."""
 
     try:
-        if transaction["operationAmount"]["currency"]["code"] == currency:
-            return float(transaction.get("operationAmount").get("amount"))
-        elif transaction["operationAmount"]["currency"]["code"] == "USD":
-            return currency_conversion_in_rub(transaction)
-        elif transaction["operationAmount"]["currency"]["code"] == "EUR":
+        if transaction["operationAmount"]["currency"]["code"] == "RUB":
+            return float(transaction["operationAmount"]["currency"]["code"])
+        else:
             return currency_conversion_in_rub(transaction)
     except KeyError:
         return "Транзакция не найдена"
