7b467b5a0d9876adb9c89cb19e9201ce96e21cc4
Artem Zhitkov <aptem_deadly@mail.ru>
2024-09-28 16:38:58 +0500

    fixes read_from_file.py

--- src/read_from_file.py
+++ src/read_from_file.py
@@ -1,49 +1,53 @@
+import logging
 import os
 
 import pandas as pd
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/read_from_file.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+logger = logging.getLogger("read_from_file")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(abs_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(funcName)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 PATH_TO_CSV = os.path.join(os.path.dirname(__file__), "..", "data", "transactions.csv")
 PATH_TO_EXCEL = os.path.join(os.path.dirname(__file__), "..", "data", "transactions_excel.xlsx")
 
 
-def read_from_csv(path: str, sep: str = ";") -> list | None:
+def read_from_csv(path: str, sep: str = ";") -> list:
     """Функция, которая принимает на вход путь к файлу с транзакциями в формате .csv и возвращает
     список словарей с транзакциями"""
     try:
-        if not os.path.isfile(path):
-            raise FileNotFoundError(f"Файл {path} не найден.")
-
+        logger.info(f"Чтение файла {path}")
         df = pd.read_csv(path, sep=sep)
+        logger.info("Возврат списка словарей с транзакциями")
         return df.to_dict(orient="records")
 
     except pd.errors.EmptyDataError:
-        print(f"Ошибка: Файл {path} пустой.")
-        return None
-    except pd.errors.ParserError as e:
-        print(f"Ошибка парсинга: {e}")
-        return None
-    except Exception as e:
-        print(f"Неожиданная ошибка: {e}")
-        return None
+        logger.warning(f"Ошибка: Файл {path} пустой.")
+        return []
+    except FileNotFoundError:
+        logger.warning(f"Файл {path} не найден.")
+        return []
 
 
-def read_from_excel(path: str, sheet_name: int = 0) -> list | None:
+def read_from_excel(path: str, sheet_name: int = 0) -> list:
     """Функция, которая принимает на вход путь к excel-файлу с транзакциями и возвращает
     список словарей с транзакциями"""
     try:
-        if not os.path.isfile(path):
-            raise FileNotFoundError(f"Файл {path} не найден.")
-
+        logger.info(f"Чтение файла {path}")
         df = pd.read_excel(path, sheet_name=sheet_name)
+        logger.info("Возврат списка словарей с транзакциями")
         return df.to_dict(orient="records")
 
     except pd.errors.EmptyDataError:
-        print(f"Ошибка: Лист {sheet_name} в файле {path} пустой.")
-        return None
+        logger.warning(f"Ошибка: Лист {sheet_name} в файле {path} пустой.")
+        return []
 
-    except Exception as e:
-        print(f"Неожиданная ошибка при чтении Excel файла: {e}")
-        return None
-
-
-print(read_from_csv(PATH_TO_CSV))
+    except FileNotFoundError:
+        logger.warning(f"Файл {path} не найден.")
+        return []
